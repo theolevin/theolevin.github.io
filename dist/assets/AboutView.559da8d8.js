10ff0bfd72fc500c43a738c02e8ea740997b3b9e
Zeph Levin <zlevin@gnyha.org>
2022-11-29 20:02:18 -0500

    added dist directory

---
+++ dist/assets/AboutView.559da8d8.js
@@ -0,0 +1,3 @@
+import{_ as t,o,c as n}from"./index.cbdbfb16.js";const a={title:"About me",body:`
+    <h1>My favorite food is PANCAKES!</h1>
+`},s=a,c={data(){return{aboutPage:s}}};const _=["innerHTML"];function r(u,i,b,d,e,l){return o(),n("div",{class:"about",innerHTML:e.aboutPage.body},null,8,_)}const f=t(c,[["render",r]]);export{f as default};
